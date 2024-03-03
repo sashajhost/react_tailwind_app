7da15d868d722753d2f93a2d32e6732653308932
sasheed <e19271@eng.pdn.ac.lk>
2024-03-04 02:42:16 +0530

    built offers component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from './Components/Hero';
 import Navbar from './Components/Navbar';
+import Offers from './Components/Offers';
 
 const App = () => {
   return (
     <div className=''>
       <Navbar/>
       <Hero/>
+      <Offers/>
       
       
     </div>
