f30df38c7eec6d281e38f4eabadbc1f36745325e
sasheed <e19271@eng.pdn.ac.lk>
2024-03-04 04:00:03 +0530

    make resposnsive , make all items center in mobiles

--- src/Components/Plan.js
+++ src/Components/Plan.js
@@ -27,7 +27,7 @@ const Plan = () => {
                  outside the hotel for exploring the area. Enjoy our outdoor pool, karaoke bar, and on-site restaurants.</p>
         </div>
 
-        <div className='flex items-center justify-center'>
+        <div className='flex items-center justify-center md:flex md:justify-normal'>
             <button class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">Learn More</button>
             <button class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">Book Your Trip</button>
         </div>
