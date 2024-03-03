d8efb31405f64bc310743b840782f8eaa389c318
sasheed <e19271@eng.pdn.ac.lk>
2024-03-04 00:17:10 +0530

    add padding to text and button container

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -7,13 +7,13 @@ const Hero = () => {
         <img className='top-0 left-0 object-cover w-full h-screen' src={hotel} alt="hotel front"/>
         <div className='absolute top-0 left-0 w-full h-screen bg-black/30' /> 
 
-        <div className='absolute top-0 flex flex-col justify-center w-full h-full text-center text-white md:text-left'>
+        <div className='absolute top-0 flex flex-col justify-center w-full h-full text-center text-white md:text-left p-11'>
             <div className='md:left-[10%] max-w-[1100px] m-auto p-4 '>
             <p className=''>All inclussive</p>
             <h1 className='text-3xl font-bold md:text-5xl drop-shadow-2xl'>Private beaches and gateaways</h1>
              <p className='max-w-[600px] drop-shadow-2xl py-2 text-xl'>This accommodation is a 1-minute walk from the beach. Located 12 km from Colombo, Saprin Beach Resort offers an outdoor pool and a private beach area.
                  The hotel has a sun terrace and views of the sea. Free private parking is available on site.</p>
-            <button className='px-3 py-2 text-black transition duration-100 ease-linear transform rounded-full hover:scale-110 hover:bg-blue-600 bg-sky-400'>Reserve Now</button>
+            <button className='px-3 py-2 text-white transition duration-100 ease-linear transform rounded-full hover:scale-110 hover:bg-blue-600 bg-sky-400'>Reserve Now</button>
             </div>
             
         </div>
