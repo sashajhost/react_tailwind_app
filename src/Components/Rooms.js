a7c9d5a6c99361b28ff9c354673c087d847352d6
sasheed <e19271@eng.pdn.ac.lk>
2024-03-05 01:03:29 +0530

    responsive design for mobile

--- src/Components/Rooms.js
+++ src/Components/Rooms.js
@@ -6,15 +6,15 @@ import Room3 from '../Assets/Room3.jpg';
 const Rooms = () => {
   return (
     <div className='max-w-[1400px] h-[500px] bg-blue-100 mx-auto my-20 pt-16 lg-[20%] md:mb-[35%] grid lg:grid-cols-3 gap-4'>
-        <div className='relative col-span-2 lg:top-20 lg:col-span-1 '>
-            <h3 className='text-2xl font-bold '>Fine Interior Rooms</h3>
-            <p className='pt-4 '>This double room's standout feature is the pool with a view. The spacious double 
+        <div className='relative col-span-2 g:top-20 lg:col-span-1 ps-4'>
+            <h3 className='text-2xl font-bold text-center md:text-left'>Fine Interior Rooms</h3>
+            <p className='pt-4 text-center md:text-left'>This double room's standout feature is the pool with a view. The spacious double 
                 room offers air conditioning, a minibar, a terrace with garden views as well as 
                 a private bathroom featuring a walk-in shower. The unit offers 1 bed.</p>
 
         </div>
 
-        <div className='grid grid-cols-2 col-span-2 gap-2'>
+        <div className='grid grid-cols-2 col-span-2 gap-2 pr-4 ps-4 '>
             <img className='object-cover w-full h-full' src={Room1} alt=''/>
             <img className='object-cover w-full h-full row-span-2' src={Room2} alt=''/>
             <img className='object-cover w-full h-full' src={Room3} alt=''/>
