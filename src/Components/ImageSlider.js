ad471c790be50263bb6d80ac78a05c404aec2641
sasheed <e19271@eng.pdn.ac.lk>
2024-03-05 03:42:41 +0530

    initialy complete slider component

--- src/Components/ImageSlider.js
+++ src/Components/ImageSlider.js
@@ -1,40 +1,66 @@
-import React from 'react';
-import interior1 from '../Assets/interior1.jpg'; // Corrected spelling to 'interior'
+import React, { useState } from 'react';
+import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
+import { RxDotFilled } from 'react-icons/rx';
+
+// Import local images
+import interior1 from '../Assets/interior1.jpg';
 import interior2 from '../Assets/interior2.jpg';
 import interior3 from '../Assets/interior3.jpg';
 import interior4 from '../Assets/interior4.jpg';
-import { IoIosArrowBack } from "react-icons/io";
-import { IoIosArrowForward } from "react-icons/io";
-
 
 const ImageSlider = () => {
   const slides = [
-    { image: interior1 }, // Use 'image' property for clarity
-    { image: interior2 },
-    { image: interior3 },
-    { image: interior4 },
+    { url: interior1, title: 'Interior Design 1' },
+    { url: interior2, title: 'Interior Design 2' },
+    { url: interior3, title: 'Interior Design 3' },
+    { url: interior4, title: 'Interior Design 4' },
   ];
 
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  };
+
   return (
-    <div>
-      <div className='max-w-[1400px] h-[500px] w-full m-auto py-16 px-4 relative lg:h-screen '>
-        <img className='w-full h-full duration-500 bg-center bg-cover rounded-2xl ' src={slides[3].image} alt="Interior Design 1" style={{ width: '100%', height: '100%' }} /> {/* Add alt text and styles */}
+    <div className='max-w-[1400px] h-[580px] w-full m-auto py-16 px-4 relative group'>
+      <div
+        className='w-full h-full duration-500 bg-center bg-cover rounded-2xl'
+        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+      ></div>
+      {/* Left Arrow */}
+      <div className='absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 group-hover:bg-black/20 text-white cursor-pointer'>
+        <BsChevronCompactLeft onClick={prevSlide} size={30} />
+      </div>
+      {/* Right Arrow */}
+      <div className='absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 group-hover:bg-black/20 text-white cursor-pointer'>
+        <BsChevronCompactRight onClick={nextSlide} size={30} />
       </div>
 
-      {/*left arrow*/}
-      <div className='absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/30 '>
-       <IoIosArrowBack />
-        
+      <div className='flex justify-center py-2 top-4'>
+        {slides.map((slide, slideIndex) => (
+          <div
+            className='text-2xl cursor-pointer'
+            key={slideIndex}
+            onClick={() => goToSlide(slideIndex)}
+          >
+            <RxDotFilled />
+          </div>
+        ))}
       </div>
-
-      {/*right arrow*/}
-      <div>
-       <IoIosArrowForward />
-        
-      </div>
-      
     </div>
-    
   );
 };
 
