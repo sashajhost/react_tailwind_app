9da5bb7f4cc457874d23788a6267ca1243860ed9
sasheed <e19271@eng.pdn.ac.lk>
2024-03-05 22:40:10 +0530

    built footer compoennt

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,12 +1,74 @@
-import React from 'react'
+import React from 'react';
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
+import { TiSocialPinterest } from 'react-icons/ti';
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className='w-full px-2 py-2 mt-24 text-gray-300 bg-gray-900'>
+      <div className='max-w-[1400px] mx-auto grid grid-cols-2 md:grid-cols-6 border-b-2 border-gray-600 py-8 px-4'>
+        <div>
+          <h6 className='pt-2 font-bold uppercare'>Solutions</h6>
+          <ul>
+            <li className='py-1'>Travel</li>
+            <li className='py-1'>Booking</li>
+            <li className='py-1'>Flights</li>
+            <li className='py-1'>Cruises</li>
+            <li className='py-1'>Ground</li>
+          </ul>
+        </div>
+        <div>
+          <h6 className='pt-2 font-bold uppercare'>Support</h6>
+          <ul>
+            <li className='py-1'>Pricing</li>
+            <li className='py-1'>Documentation</li>
+            <li className='py-1'>Tours</li>
+            <li className='py-1'>Refunds</li>
+          </ul>
+        </div>
+        <div>
+          <h6 className='pt-2 font-bold uppercare'>Company</h6>
+          <ul>
+            <li className='py-1'>About</li>
+            <li className='py-1'>Blog</li>
+            <li className='py-1'>Jobs</li>
+            <li className='py-1'>Press</li>
+            <li className='py-1'>Partners</li>
+          </ul>
+        </div>
+        <div>
+          <h6 className='pt-2 font-bold uppercare'>Legal</h6>
+          <ul>
+            <li className='py-1'>Claims</li>
+            <li className='py-1'>Privacy</li>
+            <li className='py-1'>Terms</li>
+            <li className='py-1'>Policies</li>
+            <li className='py-1'>Conditions</li>
+          </ul>
+        </div>
+        <div className='col-span-2 py-8 md:pt-2'>
+          <p className='font-bold uppercase'>Subscribe to our newsletters</p>
+          <p className='py-4'>
+            The Latest deals, articles and resources sent to your inbox weekly.
+          </p>
+          <form className='flex flex-col sm:flex-row'>
+            <input className='w-full p-2 mb-4 mr-4 rounded-md' type='email' placeholder='Enter email..' />
+            <button className='p-2 mb-4 rounded-md'>Subscribe</button>
+          </form>
+        </div>
+      </div>
 
-export default Footer
+      <div className='flex flex-col max-w-[1400px] px-2 py-4 mx-auto justify-between sm:flex-row text-center text-gray-500'>
+        <p className='py-4'>2022 Experiences, LLC. All rights reserved</p>
+        <div className='flex justify-between sm:w-[300px] pt-4 text-2xl'>
+          <FaFacebook />
+          <FaInstagram />
+          <FaTwitter />
+          <TiSocialPinterest size={30} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
 
